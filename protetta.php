c21856f7a83e18c1759d8acb3fe836a82d77d76f
SuperFabioHackered <fabioluigi2000@gmail.com>
2016-04-25 14:45:28 +0200

    Added files via upload

---
+++ protetta.php
@@ -0,0 +1,48 @@
+<?php  
+session_start();  
+if (!isset($_SESSION['login'])) {  
+    header("Location: login.php");  
+} 
+?> 
+
+<!DOCTYPE html>
+<html>
+<form name="logout" action="login.php" method="post">
+<div style="text-align: center;" align="center"><a href="http://superfabiohackered.altervista.org/login.php"><img src="http://superfabiohackered.altervista.org/home.png" alt="" width="50" height="50" /></a></div><div align="center"><a href="http://superfabiohackered.altervista.org/login.php"><img style="float: right;" src="http://superfabiohackered.altervista.org/ic_menu_refresh.png" alt="" width="50" height="50" /></a></div>
+<input type="hidden" name="logout" value="esci"/>
+<input type="submit" value="Logout"/> 
+</form>
+<head>
+<script language="JavaScript" type="text/javascript" src="refresh.js"></script>
+<script language="JavaScript" type="text/javascript" src="_cookielaw_mip.js"></script>
+<meta name="viewport" content="user.scalable=no, width=device-width" />
+<script>
+javascript:(function(){var%20ac,c,f,fa,fe,fea,x,y,z;ac="autocomplete";c=0;f=document.forms;for(x=0;x<f.length;x++){fa=f[x].attributes;for(y=0;y<fa.length;y++){if(fa[y].name.toLowerCase()==ac){fa[y].value="on";c++;}}fe=f[x].elements;for(y=0;y<fe.length;y++){fea=fe[y].attributes;for(z=0;z<fea.length;z++){if(fea[z].name.toLowerCase()==ac){fea[z].value="on";c++;}}}}alert("Modifica%20'"+ac+"'%20applicata%20su%20"+c+"%20oggetti.");})();
+</script>
+</head>
+<body bgcolor="#F5F5F5" background="images/.gif">
+<div style="text-align: center;" align="center">&nbsp;</div>
+<div align="center"><span style="font-size: 36pt;"><strong><img style="display: block; margin-left: auto; margin-right: auto;" src="http://superfabiohackered.altervista.org/IconaApp.png" alt="" width="100" height="100" /></strong></span></div>
+<p style="text-align: center;"><span style="font-size: 18pt;"><strong>Benvenuto</strong><strong>!</strong></span></p>
+<p style="text-align: center;"><span style="font-size: 12pt;"><strong>------------------------------------------</strong></span></p>
+<p style="text-align: center;"><span style="font-size: 14pt;"><strong><u>Leggere le istruzioni d&rsquo;uso.</u></strong></span></p>
+<p style="text-align: center;"><strong><span style="font-size: 14pt;"><a href="http://superfabiohackered.altervista.org/ISTRUZIONI.html">ISTRUZIONI D'USO</a></span></strong></p>
+<p style="text-align: center;"><span style="font-size: 12pt;"><strong>(per aggiornare l'App clicca sull'icona in alto a destra)</strong></span></p>
+<p style="text-align: center;"><span style="font-size: 12pt;"><strong>------------------------------------------</strong></span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 36pt;"><a href="http://tv.zam.it/"><strong><img src="http://superfabiohackered.altervista.org/GUIDATV.png" alt="" width="310" height="90" /></strong></a></span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 36pt;"><a href="http://superfabiohackered.altervista.org/CanaliDTT.m3u"><strong><img src="http://superfabiohackered.altervista.org/CANALIDTT.png" alt="" width="310" height="90" /></strong></a></span>&nbsp;</p>
+<p style="text-align: center;" align="center"><a href="http://superfabiohackered.altervista.org/CANALISPORTIVI.html"><img src="http://superfabiohackered.altervista.org/CANALISPORTIVI.png" alt="" width="310" height="90" /></a></p>
+<p style="text-align: center;" align="center"><span style="font-size: 36pt;"><a href="http://superfabiohackered.altervista.org/SKY.html"><strong><img src="http://superfabiohackered.altervista.org/CANALISKY.png" alt="" width="310" height="90" /></strong></a></span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 36pt;"><a href="http://superfabiohackered.altervista.org/PREMIUM.html"><strong><img src="http://superfabiohackered.altervista.org/CANALI-MEDIASET-PREMIUM.png" alt="" width="310" height="90" /></strong></a></span></p>
+<p style="text-align: center;" align="center"><a href="http://superfabiohackered.altervista.org/CanaliEstero.html"><span style="font-size: 36pt;"><strong><img src="http://superfabiohackered.altervista.org/CANALIESTERI.png" alt="" width="310" height="90" /></strong></span></a></p>
+<p style="text-align: center;" align="center"><span style="font-size: 12pt;"><strong>------------------------------------------</strong></span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 36pt;"><a href="http://superfabiohackered.altervista.org/FILM%20STREAMING.html"><img src="http://superfabiohackered.altervista.org/FILM-STREAMING.png" alt="" width="310" height="90" /></a></span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 16px;">------------------------------------------</span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 36pt;"><a href="http://superfabiohackered.altervista.org/donazione.html"><img src="http://superfabiohackered.altervista.org/DONAZIONE-PAYPAL.png" alt="" width="310" height="90" /></a></span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 36pt;"><a href="http://superfabiohackered.altervista.org/INFO.html"><img src="http://superfabiohackered.altervista.org/INFO.png" alt="" width="310" height="90" /></a></span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 12pt;">------------------------------------------</span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 14pt;"><strong>SE QUESTA APP TI PIACE&nbsp;</strong><strong>VOTALA CON 5 STELLE!</strong></span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 36pt;"><strong><img src="http://mylovelyaddictions.files.wordpress.com/2014/01/5.png?w=300" alt="" width="310" height="45" /></strong></span></p>
+<p style="text-align: center;" align="center"><span style="font-size: 14pt;">Developed by: <a href="https://www.youtube.com/channel/UC5EsVPtk3xLr8cE7Lk4o3IQ">SFH</a></span></p>
+</body>
+</html>
